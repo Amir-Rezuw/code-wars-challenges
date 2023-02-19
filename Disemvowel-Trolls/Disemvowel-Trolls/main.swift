12c0d42923db52d35230fa5743e6062cdc4dcece
AmirReza Jamali <amir7jamali.ar@yahoo.com>
2023-02-19 22:44:13 +0330

    Quarter of the year

--- Disemvowel-Trolls/Disemvowel-Trolls/main.swift
+++ Disemvowel-Trolls/Disemvowel-Trolls/main.swift
@@ -6,6 +6,16 @@
 //
 
 import Foundation
+import RegexBuilder
 
-print("Hello, World!")
+func disemvowel(_ s: String) -> String {
 
+    // If you're using swift 5.7 or later comment mentioned one and replace it with these three they're same but this one is more readable
+    //    let regex = Regex { One(.anyOf("eEaAOouUiI")) } uncomment this
+    let regex = try! Regex("[AaEeIiOoUu]") // comment this
+    var result = s
+    let matches = s.matches(of: regex)
+    result.replace(regex, with: "")
+    return result
+}
+print(disemvowel("Hello worldi"))
---
+++ Quarter-of-the-year/Quarter-of-the-year/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Quarter-of-the-year
+//
+//  Created by AmirReza Jamali on 2/19/23.
+//
+
+import Foundation
+
+print("Hello, World!")
+
